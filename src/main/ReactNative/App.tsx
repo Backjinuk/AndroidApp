56a5a3dba293544693a4c9a409b50335225cfa23
BJW <backj123@naver.com>
2024-06-26 23:43:40 +0900

    :rocket:  [BJW] 2024/06/24 회원가입 UI 수정

--- src/main/ReactNative/App.tsx
+++ src/main/ReactNative/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import JoinFormMain from "./Component/JoinComponent/JoinFormMain.tsx";
 import LoginMain from "./Component/LoginComponent/LoginMain.tsx";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+    Login: undefined;
+    Join: undefined;
+};
 
 
 function App(): React.JSX.Element {
+    const Stack = createNativeStackNavigator<RootStackParamList>();
 
     return (
-        /*<JoinFormMain/>*/
-        <LoginMain/>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name="Join" component={JoinFormMain}/>
+                <Stack.Screen name="Login" component={LoginMain}/>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
 
 export default App;
--- src/main/ReactNative/Component/LoginComponent/LoginMain.tsx
+++ src/main/ReactNative/Component/LoginComponent/LoginMain.tsx
@@ -1,12 +1,22 @@
 import {Image, Text, TouchableOpacity, View} from "react-native";
 import React from "react";
 import createStyles from "./styles.ts";
+import {useNavigation} from "@react-navigation/native";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+
+type RootStackParamList = {
+    Login: undefined;
+    Join: undefined;
+};
+
 
 export default function LoginMain(){
     const styles = createStyles();
+    const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+    // @ts-ignore
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>The JoongAng</Text>
+            <Text style={styles.title}>MoGakCo</Text>
             <Text style={styles.subtitle}>회원가입</Text>
             <Text style={styles.description}>회원가입 방식을 선택해 주세요.</Text>
 
@@ -30,14 +40,13 @@ export default function LoginMain(){
 
             <Text style={styles.orText}>또는</Text>
 
-            <TouchableOpacity style={styles.signupButton}>
-                <Text style={styles.signupButtonText}>조인스로 회원가입</Text>
+            <TouchableOpacity style={styles.signupButton} onPress={ () => { navigation.navigate("Join"); }}>
+                <Text style={styles.signupButtonText}>모각코로 회원가입</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.signupButton}>
                 <Text style={styles.signupButtonText}>이메일로 회원가입</Text>
             </TouchableOpacity>
 
-            {/*<TouchableOpacity onPress={() => navigation.navigate('Login')}>*/}
             <TouchableOpacity>
                 <Text style={styles.loginText}>이미 회원이신가요? 로그인</Text>
             </TouchableOpacity>
