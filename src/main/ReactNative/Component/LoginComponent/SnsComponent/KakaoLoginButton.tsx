aadf83e2b89c35238b7050b6e6053f7014514db9
BJW <backj123@naver.com>
2024-07-01 22:53:48 +0900

    :rocket:  [BJW] 2024/07/01 카카오로그인 구현

--- src/main/ReactNative/Component/LoginComponent/SnsComponent/KakaoLoginButton.tsx
+++ src/main/ReactNative/Component/LoginComponent/SnsComponent/KakaoLoginButton.tsx
@@ -1,20 +1,52 @@
 import React, {useState} from 'react';
 import {TouchableOpacity, Image, Alert} from 'react-native';
 import {login, logout, unlink} from "@react-native-seoul/kakao-login";
+import Config from "react-native-config";
+import axios from "axios";
+import {useNavigation} from "@react-navigation/native";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {RootStackParamList} from "../../../CommonTypes/RootStackParamList.ts";
+import {jwtDecode} from "jwt-decode";
 
 // @ts-ignore
 export default function KakaoLoginButton({ styles }){
     const [result, setResult] = useState<string>("");
+    const api = Config.API_BASE_URL
+    const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
     const signInWithKakao = async (): Promise<void> => {
         try {
             const token = await login();
             setResult(JSON.stringify(token));
-            console.log('Token:', token);
+
+            LoginAxois();
+
         } catch (err) {
             console.error("login err", err);
         }
     };
+
+    const LoginAxois = () => {
+        const value = jwtDecode(result);
+
+        axios.post(api+"/user/userJoin", JSON.stringify({
+            userId : value.aud,
+            // @ts-ignore
+            userName : value.nickname,
+            userType : "Kakao"
+        }), {
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        }).then(res => {
+            if(res.data){
+                Alert.alert("회원가입이 완료 되었습니다.")
+            }else{
+                navigation.navigate("MapMain")
+            }
+        })
+
+    }
 
     const signOutWithKakao = async (): Promise<void> => {
         try {
--- src/main/ReactNative/android/app/src/main/java/com/reactnative/MainActivity.kt
+++ src/main/ReactNative/android/app/src/main/java/com/reactnative/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.reactnative
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+
 
 class MainActivity : ReactActivity() {
 
@@ -19,4 +28,37 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+
+        //setContentView(R.layout.activity_main);
+        getHashKey()
+    }
+
+    private fun getHashKey() {
+        val packageInfo: PackageInfo? = try {
+            packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+            null
+        }
+        if (packageInfo == null) {
+            Log.e("KeyHash", "KeyHash:null")
+            return
+        }
+
+        for (signature in packageInfo.signatures) {
+            try {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
+        }
+    }
+
 }
+
+
