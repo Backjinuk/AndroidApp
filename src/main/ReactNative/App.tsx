6900685ef2a2ac45ef828d791f616da9eecfd314
LeeJongHa <qrwrafsf@naver.com>
2024-07-01 19:42:26 +0900

    [LJH] 네이버 지도 띄우기 및 마커 찍기

--- src/main/ReactNative/App.tsx
+++ src/main/ReactNative/App.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
-import JoinFormMain from "./Component/JoinComponent/JoinFormMain.tsx";
-import LoginMain from "./Component/LoginComponent/LoginMain.tsx";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginForm from "./Component/LoginComponent/LogionForm.tsx";
-import {RootStackParamList} from "./CommonTypes/RootStackParamList.ts";
-import MapMain from "./Component/MapCompoent/MapMain.tsx";
-
-
-
+import JoinFormMain from './Component/JoinComponent/JoinFormMain.tsx';
+import LoginMain from './Component/LoginComponent/LoginMain.tsx';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginForm from './Component/LoginComponent/LogionForm.tsx';
+import {RootStackParamList} from './CommonTypes/RootStackParamList.ts';
+import MapMain from './Component/MapCompoent/MapMain.tsx';
 
 function App(): React.JSX.Element {
-    const Stack = createNativeStackNavigator<RootStackParamList>();
+  const Stack = createNativeStackNavigator<RootStackParamList>();
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="LoginMain">
-                <Stack.Screen name="Join" component={JoinFormMain}/>
-                <Stack.Screen name="LoginMain" component={LoginMain}/>
-                <Stack.Screen name="LoginForm" component={LoginForm}/>
-                <Stack.Screen name={"MapMain"} component={MapMain} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MapMain">
+        <Stack.Screen name="Join" component={JoinFormMain} />
+        <Stack.Screen name="LoginMain" component={LoginMain} />
+        <Stack.Screen name="LoginForm" component={LoginForm} />
+        <Stack.Screen name="MapMain" component={MapMain} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
--- src/main/ReactNative/Component/MapCompoent/MapMain.tsx
+++ src/main/ReactNative/Component/MapCompoent/MapMain.tsx
@@ -1,9 +1,79 @@
-import {Text} from "react-native";
+import React, {useState} from 'react';
+import {Button, View} from 'react-native';
+import {
+  Camera,
+  NaverMapMarkerOverlay,
+  NaverMapView,
+} from '@mj-studio/react-native-naver-map';
+import axios, {AxiosError} from 'axios';
+import Config from 'react-native-config';
 
 export default function MapMain() {
-    return (
-        <>
-            <Text>메인페이지 입니다.</Text>
-        </>
-    )
+  const [camera, setCamera] = useState<Camera>({
+    latitude: 37.50497126,
+    longitude: 127.04905021,
+    zoom: 15,
+  });
+  const naver_search_api_client_id = Config.NAVER_SEARCH_API_CLIENT_ID;
+  const naver_search_api_client_secret = Config.NAVER_SEARCH_API_CLIENT_SECRET;
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  const test = async () => {
+    console.log('start');
+    console.log(camera);
+    // try {
+    //   const data = await axios.get(
+    //     'https://openapi.naver.com/v1/search/local.json',
+    //     {
+    //       params: {
+    //         query: '다이소 신림동',
+    //         display: 5,
+    //       },
+    //       headers: {
+    //         'X-Naver-Client-Id': naver_search_api_client_id, // 여기에 네이버 개발자 센터에서 발급받은 Client ID를 입력하세요
+    //         'X-Naver-Client-Secret': naver_search_api_client_secret, // 여기에 네이버 개발자 센터에서 발급받은 Client Secret을 입력하세요
+    //       },
+    //     },
+    //   );
+    //   console.log(data.data);
+    //   console.log();
+    //   data.data.items.forEach((element: any) => {
+    //     console.log(element);
+    //     console.log(element.address);
+    //     console.log(element.title);
+    //     console.log();
+    //   });
+    // } catch (error) {
+    //   if (error instanceof AxiosError) console.log(error.response);
+    // }
+    console.log('end');
+  };
+  return (
+    <>
+      <Button title="search" onPress={test} />
+      <View style={{flex: 1}}>
+        <NaverMapView
+          style={{flex: 1}}
+          initialCamera={camera}
+          onCameraChanged={c => {
+            setCamera(c);
+          }}
+          onTapMap={params => {
+            setLat(params.latitude);
+            setLon(params.longitude);
+          }}>
+          {lat !== 0 && (
+            <NaverMapMarkerOverlay
+              latitude={lat}
+              longitude={lon}
+              onTap={() => {
+                setLat(0);
+              }}
+              anchor={{x: 0.5, y: 1}}
+            />
+          )}
+        </NaverMapView>
+      </View>
+    </>
+  );
 }
