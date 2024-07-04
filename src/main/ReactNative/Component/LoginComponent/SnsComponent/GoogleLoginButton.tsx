4b27d2ac2fcf02563b72f83006247b62ed6f82ec
BJW <backj123@fgi.kr>
2024-07-04 18:47:55 +0900

    :zap: [BJW] 구글 API를 활용하여 로그인 구현_1

--- src/main/ReactNative/Component/LoginComponent/SnsComponent/GoogleLoginButton.tsx
+++ src/main/ReactNative/Component/LoginComponent/SnsComponent/GoogleLoginButton.tsx
@@ -1,10 +1,39 @@
 import {Image, TouchableOpacity} from "react-native";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import Config from "react-native-config";
 
 // @ts-ignore
 export default function GoogleLoginButton({styles}) {
+    const [token, setIdToken] = useState('');
+
+    useEffect(() => {
+        GoogleSignin.configure({
+            webClientId: Config.GOOGLE_CLIENT_ID, // Firebase 콘솔에서 가져온 웹 클라이언트 ID
+        });
+    }, []);
+
+    const onPressGoogleBtn = async () => {
+
+        try {
+            await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+            const {idToken} = await GoogleSignin.signIn();
+            console.log('idToekn : ', idToken);
+            if (idToken) {
+                setIdToken(idToken);
+            }
+            const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+            const res = await auth().signInWithCredential(googleCredential);
+        }catch(error){
+            console.error(error);
+        }
+    };
     return (
-        <TouchableOpacity style={styles.socialButton}>
+        <TouchableOpacity style={styles.socialButton} onPress={() => onPressGoogleBtn()}>
             <Image source={require('../assets/google.png')} style={styles.icon} />
         </TouchableOpacity>
     )
