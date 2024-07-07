86f41c8e7fdb6c45d4f4ea7a5e15989f83af54ed
BJW <backj123@naver.com>
2024-07-07 17:23:05 +0900

    :rocket:  [BJW] 2024/07/07 페이스북 로그인 구현

--- src/main/ReactNative/Component/LoginComponent/SnsComponent/FaceBookLoginButton.tsx
+++ src/main/ReactNative/Component/LoginComponent/SnsComponent/FaceBookLoginButton.tsx
@@ -1,11 +1,36 @@
-import {Image, TouchableOpacity} from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Alert, Image, TouchableOpacity } from "react-native";
+import {AccessToken, LoginManager} from "react-native-fbsdk-next";
 
 // @ts-ignore
-export default function FaceBookLoginButton({styles}) {
+export default function FaceBookLoginButton({ styles }) {
+    const [token, setToken] = useState('');
+
+    const facebookLogin = async () => {
+        try {
+            const result = await LoginManager.logInWithPermissions(['public_profile']);
+
+            if (result.isCancelled) {
+                console.log('Login canceled');
+            } else {
+                const tokenData = await AccessToken.getCurrentAccessToken();
+                if (tokenData) {
+                    const accessToken = tokenData.accessToken.toString();
+                    console.log('Access Token:', accessToken);
+                    // 여기서 accessToken을 이용하여 추가적인 작업 수행 가능
+                } else {
+                    Alert.alert('Login failed', 'Unable to get access token');
+                }
+            }
+        } catch (error) {
+            // @ts-ignore
+            Alert.alert('Login error', error.message);
+        }
+    };
+
     return (
-        <TouchableOpacity style={styles.socialButton}>
+        <TouchableOpacity style={styles.socialButton} onPress={() => facebookLogin()}>
             <Image source={require('../assets/facebook.png')} style={styles.icon} />
         </TouchableOpacity>
-    )
+    );
 }
--- src/main/ReactNative/Component/LoginComponent/SnsComponent/GoogleLoginButton.tsx
+++ src/main/ReactNative/Component/LoginComponent/SnsComponent/GoogleLoginButton.tsx
@@ -1,9 +1,6 @@
 import {Image, TouchableOpacity} from "react-native";
 import React, {useEffect, useState} from "react";
-import {
-    GoogleSignin,
-    GoogleSigninButton,
-} from '@react-native-google-signin/google-signin';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
 import Config from "react-native-config";
 
@@ -12,28 +9,42 @@ export default function GoogleLoginButton({styles}) {
     const [token, setIdToken] = useState('');
 
     useEffect(() => {
-        GoogleSignin.configure({
-            webClientId: Config.GOOGLE_CLIENT_ID, // Firebase 콘솔에서 가져온 웹 클라이언트 ID
-        });
+        googleSigninConfigure();
     }, []);
 
-    const onPressGoogleBtn = async () => {
 
+    const googleSigninConfigure = () => {
+        console.log(Config.GOOGLE_CLIENT_ID)
+        GoogleSignin.configure({
+            webClientId: Config.GOOGLE_CLIENT_ID, // Firebase 콘솔에서 가져온 웹 클라이언트 ID
+            offlineAccess: true, // 필요한 경우 오프라인 액세스 설정
+            forceCodeForRefreshToken: true, // 필요한 경우 리프레시 토큰 강제 설정
+            hostedDomain: '',
+        });
+
+    }
+
+    const onGoogleButtonPress = async () => {
         try {
-            await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-            const {idToken} = await GoogleSignin.signIn();
-            console.log('idToekn : ', idToken);
-            if (idToken) {
-                setIdToken(idToken);
-            }
+            console.log("Checking for Google Play Services...");
+            await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+            console.log("Google Play Services available.");
+            console.log("Attempting Google Sign-In...");
+            const { idToken } = await GoogleSignin.signIn();
+            console.log("Google Sign-In successful. ID Token: ", idToken);
             const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-            const res = await auth().signInWithCredential(googleCredential);
-        }catch(error){
-            console.error(error);
+            console.log("Google Credential created.");
+            return auth().signInWithCredential(googleCredential);
+        } catch (e) {
+            console.error("Error during Google Sign-In process: ", e);
         }
-    };
+    }
+
+
+
+
     return (
-        <TouchableOpacity style={styles.socialButton} onPress={() => onPressGoogleBtn()}>
+        <TouchableOpacity style={styles.socialButton} onPress={() => onGoogleButtonPress()}>
             <Image source={require('../assets/google.png')} style={styles.icon} />
         </TouchableOpacity>
     )
